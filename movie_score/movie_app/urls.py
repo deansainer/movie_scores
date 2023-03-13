ce03dbae7920e1cf27ff5750462de25f5dd3a6ea
deansainer <determind2k@gmail.com>
2023-03-14 00:14:42 +0100

    commit

--- movie_score/movie_app/urls.py
+++ movie_score/movie_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('', views.movie_list, name='movie_list')
+    path('', views.movie_list, name='movie_list'),
+    path('series_list/', views.series_list, name='series_list')
 ]
+
--- movie_score/movie_app/views.py
+++ movie_score/movie_app/views.py
@@ -6,3 +6,9 @@ def movie_list(request):
     movies = Movie.objects.all()
     context = {'movies': movies}
     return render(request, 'movie_app/movie_list.html', context)
+
+
+def series_list(request):
+    serieses = Series.objects.all()
+    context = {'serieses': serieses}
+    return render(request, 'movie_app/series_list.html', context)
