613b3358577bbcfc348611f1fcbc2e69684c71ec
deansainer <determind2k@gmail.com>
2023-03-18 13:43:54 +0100

    commit

--- movie_score/movie_app/urls.py
+++ movie_score/movie_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import *
 
+
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('series_list/', views.series_list, name='series_list')
